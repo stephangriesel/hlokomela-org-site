d95c41995195c2655a17ab36d51f9f43a671ce09
Stephan Griesel <sgriesel@gmail.com>
2023-05-10 18:43:44 +0200

    borderwidth add

--- custom-config.cjs
+++ custom-config.cjs
@@ -5,6 +5,24 @@ module.exports = {
 	],
 	theme: {
 		extend: {},
+		borderWidth: {
+      DEFAULT: '1px',
+      '0': '0',
+      '2': '2px',
+      '3': '3px',
+      '4': '4px',
+      '6': '6px',
+      '8': '8px',
+      '10': '10px',
+      '12': '12px',
+      '14': '14px',
+      '16': '16px',
+      '18': '18px',
+      '20': '20px',
+      '22': '22px',
+      '24': '24px',
+      '26': '26px',
+    }
 	},
 	plugins: [
 		require('flowbite/plugin')
