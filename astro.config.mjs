45835c9f6d7a4e9371f4b16b07fa83070a48be6c
Stephan Griesel <sgriesel@gmail.com>
2023-06-13 06:32:19 +0200

    config clean up

--- astro.config.mjs
+++ astro.config.mjs
@@ -19,37 +19,6 @@ export default defineConfig({
         public_folder: '/upload',
         collections: [
           {
-            name: 'posts',
-            label: 'Blog Posts',
-            label_singular: 'Blog Post',
-            folder: 'src/pages/posts',
-            create: true,
-            delete: true,
-            fields: [
-              { name: 'title', widget: 'string', label: 'Post Title' },
-              {
-                name: 'publishDate',
-                widget: 'datetime',
-                format: 'DD MMM YYYY',
-                date_format: 'DD MMM YYYY',
-                time_format: false,
-                label: 'Publish Date',
-              },
-              { name: 'author', widget: 'string', label: 'Author Name', required: false },
-              { name: 'authorURL', widget: 'string', label: 'Author URL', required: false },
-              { name: 'description', widget: 'string', label: 'Description', required: false },
-              { name: 'body', widget: 'markdown', label: 'Post Body' },
-              {
-                name: 'layout',
-                widget: 'select',
-                default: '../../layouts/BlogPost.astro',
-                options: [
-                  { label: 'Blog Post', value: '../../layouts/BlogPost.astro' },
-                ],
-              },
-            ],
-          },
-          {
             name: 'media',
             label: 'Media',
             label_singular: 'Blog Post',
